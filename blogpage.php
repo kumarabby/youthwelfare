25fcfc8f02cf8c1c9bdcb75bcb75b63cae8e9d19
kumarabby <569kabhinav@gmail.com>
2017-01-22 22:09:14 +0530

    blogpage

---
+++ blogpage.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<title>Blog page</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", Arial, Helvetica, sans-serif}
+.myLink {display: none}
+</style>
+<body class="w3-light-grey">
+
+<!-- Navigation Bar -->
+<ul class="w3-navbar w3-white w3-border-bottom w3-xlarge">
+  <li><a href="#" class="w3-text-red w3-hover-red"><b><i class="fa fa-map-marker w3-margin-right"></i>youth welfare</b></a></li>
+  <li class="w3-right"><a href="#" class="w3-hover-red w3-text-grey"><i class="fa fa-search"></i></a></li>
+</ul>
+<!-- Main content: shift it to the right by 250 pixels when the sidenav is visible -->
+<div class="w3-main" style="margin-left:250px">
+
+  <div class="w3-row w3-padding-64">
+    <div class="w3-twothird w3-container">
+      <h1 class="w3-text-teal">National youth Festival 2010</h1>
+      <p> 
+Providing a forum to the young talent of the country , the 16th National Youth Festival will be held at Rajastan January 8-12, 2010.
+ The National Youth Festival is organized every year from January 12-16, to commemorate the birth anniversary of Swami Vivekananda 
+ i.e. January 12.National Youth Festival is the biggest Youth Festival of its kind, where large number of young delegates representing various States of the country interact and exhibit their cultural powers. A sizeable delegation of international youth representatives are also participating in the Festival.
+</p>
+    </div>
+    <div class="w3-third w3-container">
+      <p class="w3-border w3-padding-large w3-padding-32 w3-center">AD</p>
+      <p class="w3-border w3-padding-large w3-padding-64 w3-center">AD</p>
+    </div>
+  </div>
+
+  <div class="w3-row">
+    <div class="w3-twothird w3-container">
+      <h1 class="w3-text-teal">Keralotsavam</h1>
+      <p>Keralotsavam, a rare and unique youth festival, is conducted mainly for the youth who have no proper platform to present their cultural and physical talents. In this festival youth in the age group of 15-35 can participate in Cultural, Arts and Sports competitions in block, district level to State level. There are some selected cultural items, which are not popular in mainstream cultural scene, has also been included in this festival.
+Keralotsavam is now a widely accepted festival for the Youth of Kerala. As a result every youth in this State is getting a chance to project him/her self to the fore-front in every sphere of life.Kerala created a model for the nation by initiating a colourful youth festival “Keralotsavam”, which is conducted with gaiety and fanfare every year.
+</p>
+    </div>
+    <div class="w3-third w3-container">
+      <p class="w3-border w3-padding-large w3-padding-32 w3-center">AD</p>
+      <p class="w3-border w3-padding-large w3-padding-64 w3-center">AD</p>
+    </div>
+  </div>
+
+  <div class="w3-row w3-padding-64">
+    <div class="w3-twothird w3-container">
+      <h1 class="w3-text-teal">National Integration Camp</h1>
+      <p>India is a nation having diverse cultures, traditions, rituals and beliefs. To preserve this values and unity in diversity, it is of supreme importance that we foster National and emotional integration among our people, more particularity the youth who are the future architects of our nation.
+The Kerala State Youth Welfare Board have been conducting National Integration Camps, Youth Exchange Programs etc, which aims at promoting the National, cultural and emotional oneness among the youth.
+</p>
+    </div>
+    <div class="w3-third w3-container">
+      <p class="w3-border w3-padding-large w3-padding-32 w3-center">AD</p>
+      <p class="w3-border w3-padding-large w3-padding-64 w3-center">AD</p>
+    </div>
+  </div>
+  <div class="w3-card-2 w3-margin">
+    <div class="w3-container w3-padding">
+      <h4>Popular Links and orders</h4>
+    </div>
+    <ul class="w3-ul w3-hoverable w3-white">
+      <li class="w3-padding-16">
+        <img src="http://www.homeobook.com/wp-content/uploads/2013/08/kerala.jpg" alt="Image" class="w3-left w3-margin-right" style="width:50px">
+        <span class="w3-large">government of kerala</span><br>
+        <span>https://kerala.gov.in/</span>
+      </li>
+      <li class="w3-padding-16">
+        <img src="http://social.un.org/youthyear/images/logo/1.jpg" alt="Image" class="w3-left w3-margin-right" style="width:50px">
+        <span class="w3-large">volume1 issue 1</span><br>
+        <span>tharunam</span>
+      </li> 
+      <li class="w3-padding-16">
+        <img src="http://omojuwa.com/wp-content/uploads/2014/11/youth-logo.jpg" alt="Image" class="w3-left w3-margin-right" style="width:50px">
+        <span class="w3-large">volume2 issue 2</span><br>
+        <span>tharunam</span>
+      </li>   
+      <li class="w3-padding-16 w3-hide-medium w3-hide-small">
+        <img src="http://bobpigo.org/uploaded/partners/Cochin%20University%20Logo.jpg" alt="Image" class="w3-left w3-margin-right" style="width:50px">
+        <span class="w3-large">cochin university</span><br>
+        <span>http://www.cusat.ac.in/</span>
+      </li>  
+    </ul>
+  </div>
+  <hr> 
+
+
+
+
+<!-- Header -->
+</body>
+</html>
